22121cc7f5506d647c634c8305e69fa71451324d
tomasz.pondo@gmail.com <blueflameuser19>
2020-05-18 14:18:46 +0200

    Data&Sync Screen class and UI test

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/chapter11/screens/DataAndSyncScreen.kt
@@ -0,0 +1,17 @@
+package com.example.android.architecture.blueprints.todoapp.test.chapter11.screens
+
+class DataAndSyncScreen : BaseScreen() {
+    /*
+    ELEMENTS
+     */
+
+
+    /*
+    ACTIONS
+     */
+
+
+    /*
+    HELPERS
+     */
+}
