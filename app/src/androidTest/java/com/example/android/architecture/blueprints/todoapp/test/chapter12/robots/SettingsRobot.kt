96313873c70a19eb78deafb48d469aa11b66512f
tomasz.pondo@gmail.com <blueflameuser19>
2020-05-20 09:24:10 +0200

    RoboPattern exercise 27.1

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/chapter12/robots/SettingsRobot.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/chapter12/robots/SettingsRobot.kt
@@ -11,10 +11,10 @@ fun settings(func: SettingsRobot.() -> Unit) = SettingsRobot().apply { func() }
 class SettingsRobot {
 
     fun checkDefaultLayout() {
-        onView(allOf(withId(android.R.id.title), withText("General"))).check(matches(isCompletelyDisplayed()))
-        onView(allOf(withId(android.R.id.title), withText("Notifications"))).check(matches(isCompletelyDisplayed()))
-        onView(allOf(withId(android.R.id.title), withText("Data & sync"))).check(matches(isCompletelyDisplayed()))
-        onView(allOf(withId(android.R.id.title), withText("WebView sample"))).check(matches(isCompletelyDisplayed()))
+        onView(allOf(withId(android.R.id.title), withText(R.string.pref_header_general))).check(matches(isCompletelyDisplayed()))
+        onView(allOf(withId(android.R.id.title), withText(R.string.pref_header_notifications))).check(matches(isCompletelyDisplayed()))
+        onView(allOf(withId(android.R.id.title), withText(R.string.pref_header_data_sync))).check(matches(isCompletelyDisplayed()))
+        onView(allOf(withId(android.R.id.title), withText(R.string.pref_header_webview_sample))).check(matches(isCompletelyDisplayed()))
     }
    infix fun clickOnGeneralOption(func: GeneralRobot.() -> Unit): GeneralRobot {
         onView(allOf(withId(android.R.id.title), withText(R.string.pref_header_general)))
